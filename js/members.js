67d72087ed16ce777b978a8e63fb593d6dd7a0da
tcwongaj <tcwongaj@gmail.com>
2019-04-07 00:13:02 +0900

    Update members.js

--- js/members.js
+++ js/members.js
@@ -2,7 +2,7 @@ var app = angular.module('myApp', []);
 
 app.controller('myCtrl', function($scope, $http) {
   $scope.item= 'test';
-  $http.get('http://www.io.mei.titech.ac.jp/members.json').success(function(data) {
+  $http.get('members.json').success(function(data) {
         $scope.members = data;
 
     });
