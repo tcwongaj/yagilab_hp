4f9d182c43370e378a59cdcea04f49547f31da6f
tcwongaj <tcwongaj@gmail.com>
2019-04-10 00:36:25 +0900

    fifth

--- js/members.js
+++ js/members.js
@@ -4,22 +4,14 @@ app.controller('jsonList', function($scope, $http) {
   $http.get('members.json').then(function(response) {
     $scope.members = response.data;
   });
-
-
-
 });
 
 app.controller('urlCheck', ['$scope', function ($scope) {
-
-
   $scope.urlExist = function(url){
-
-     console.log(url);
-      if (url == null){
-        return false;
-      } else {
-        return true;
-      }
+    if (url == null){
+      return false;
+    } else {
+      return true;
     }
-
+  }
 }])
