8f31382681394245d28a18179986e776e728cf1f
tcwongaj <tcwongaj@gmail.com>
2019-04-10 02:56:39 +0900

    sixth

--- js/members.js
+++ js/members.js
@@ -1,29 +1,7 @@
 var app = angular.module('myApp', []);
 
-var n=10;
-
 app.controller('jsonList', function($scope, $http) {
   $http.get('members.json').then(function(response) {
     $scope.members = response.data;
   });
 });
-
-app.controller('urlCheck', ['$scope', function ($scope) {
-  $scope.urlExist = function(url){
-    if (url == null){
-      console.log(url);
-      console.log("false");
-      console.log(n);
-      n=n+10;
-      return false;
-    } else {
-      console.log(url);
-      console.log("true");
-      console.log(n);
-      n=n+10;
-      return true;
-    }
-  }
-
-
-}])
